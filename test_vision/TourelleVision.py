de7ea7dbe069833014180333bbaee0bd31aeb458
Felix <filabert16@outlook.con>
2022-01-30 10:54:16 -0500

    Upgrade enlever particules loins

--- test_vision/TourelleVision.py
+++ test_vision/TourelleVision.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-img = cv2.imread(r'C:\Users\plega\Downloads\FarLaunchpad7ft10inV2')
+img = cv2.imread('C:/Users/filal/Desktop/marchandises/Green/FarLaunchpad6ft0in.png')
 
 
 def but(img):
@@ -22,25 +22,26 @@ def but(img):
             if rectangularity >= 0.75:
                 x, y, w, h = cv2.boundingRect(cnt)
                 if 1.25 <= w / h <= 3.5 and w >= 10 and h >= 10:
-                    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 3)
                     validRects.append((x, y, w, h))
     validXs = []
     validYs = []
-    correctnessValue = []
-    for x, y, w, h in validRects:
-        coVal = yValye - y
-        correctnessValue.append(coVal)
-    print(correctnessValue)
-    for coVal in correctnessValue:
-        a = coVal / max(correctnessValue)
-        if a >= 0.20:
-            correctnessValue.remove(coVal)
-    print(correctnessValue)
+
     for x, y, w, h in validRects:
         xCenter = x + (w / 2)
         validXs.append(xCenter)
         yCenter = y + (h / 2)
         validYs.append(yCenter)
+
+    yAverage = int(sum(validYs) / len(validYs))
+    xAverage = int(sum(validXs) / len(validXs))
+
+    for y in validYs:
+        if abs(y - yAverage) >= 100:
+            validYs.remove(y)
+
+    for x in validXs:
+        if abs(x - xAverage) >= 300:
+            validXs.remove(x)
 
     validX = int(sum(validXs) / len(validXs))
     validY = int(sum(validYs) / len(validYs))
@@ -49,7 +50,7 @@ def but(img):
 
 
 validX, validY = but(img)
-cv2.circle(img, (validX, validY), 3, (255, 255, 0), 3)
+cv2.circle(img, (validX, validY), 3, (255, 0, 255), 3)
 cv2.imshow('img', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
