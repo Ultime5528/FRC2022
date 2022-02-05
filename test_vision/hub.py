f7c4f57b54ad602ad1e8133a9a4cefe9a0be9f05
Maxence <3800935@keranna.qc.ca>
2022-02-05 13:01:08 -0500

    Hub vision commit

--- test_vision/hub.py
+++ test_vision/hub.py
@@ -1,8 +1,6 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('C:/Users/Admin-230/Downloads/2022VisionSampleImages/intru2.png')
-
 
 class Target:
     def __init__(self, y):
@@ -15,8 +13,8 @@ class Target:
         return len(self.positions)
 
 
-def tourelle(img):
-    lowerGreen = (50, 50, 50)
+def hub(img):
+    lowerGreen = (50, 100, 160)
     highGreen = (100, 255, 255)
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lowerGreen, highGreen)
@@ -31,7 +29,7 @@ def tourelle(img):
             minRect = np.int0(cv2.boxPoints(minRect))
             minArea = cv2.contourArea(minRect)
             rectangularity = area / minArea if minArea else 0
-            if rectangularity >= 0.75:
+            if rectangularity >= 0.6:
                 x, y, w, h = cv2.boundingRect(cnt)
                 if 1.25 <= w / h <= 3.5 and w >= 10 and h >= 10:
                     validRects.append((x, y, w, h))
@@ -40,18 +38,16 @@ def tourelle(img):
     for x, y, w, h in validRects:
         xCenter = x + (w / 2)
         yCenter = y + (h / 2)
-        cv2.circle(img, (int(xCenter), int(yCenter)), 3, (0, 255, 255), 3)
         validPositions.append((xCenter, yCenter))
 
-
-
-    error = int(img.shape[0] / 10)
+    errorX = int(img.shape[1] / 10)
+    errorY = int(img.shape[0] / 10)
     targets = []
 
-    for _, targetY in validPositions:
+    for targetX, targetY in validPositions:
         target = Target(targetY)
         for x, y in validPositions:
-            if abs(targetY - y) < error:
+            if abs(targetY - y) < errorY and abs(targetX - x) < errorX:
                 target.positions.append((x, y))
                 target.error += abs(targetY - y)
         targets.append(target)
@@ -65,17 +61,6 @@ def tourelle(img):
         elif bestTarget.score <= target.score:
             bestTarget = target
 
-
-
-
     position = np.mean(bestTarget.positions, axis=0).astype("int")
 
     return position
-
-
-position = tourelle(img)
-print(position)
-cv2.circle(img, position, 3, (255, 0, 255), 3)
-cv2.imshow('img', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
