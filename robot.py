8cdf4c0e067c3f97ee94174e75f27157339f60b5
Thomas McDonald <4001137@keranna.qc.ca>
2022-02-05 13:13:10 -0500

    Added requirements.txt file for packages

--- robot.py
+++ robot.py
@@ -15,7 +15,6 @@ class Robot(commands2.TimedCommandRobot):
         self.stick = wpilib.Joystick(0)
         self.base_pilotable.setDefaultCommand(Piloter(self.base_pilotable, self.stick))
         wpilib.SmartDashboard.putData("Shoot", Shoot(self.shooter, self.stick, 0.80))
-        # TODO add shooter motor simulator
 
 if __name__ == "__main__":
     wpilib.run(Robot)
