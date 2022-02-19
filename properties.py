150ec86348f6957149d32869481aeaf83bbad94f
Étienne Beaulac <etienne.beaulac@outlook.com>
2022-02-19 14:57:54 -0500

    Add Properties class to bind to properties

--- properties.py
+++ properties.py
@@ -1,5 +1,10 @@
 from networktables.util import ntproperty
 
-viser_hub_speed = ntproperty("Properties/ViserHubSpeed", 0.3, writeDefault=False)
-viser_hub_threshold = ntproperty("Properties/ViserHubThreshold", 0.05, writeDefault=False)
-viser_hub_x_offset = ntproperty("Properties/ViserHubXOffset", 0.05, writeDefault=False)
+
+class Properties:
+    viser_hub_speed = ntproperty("Properties/ViserHubSpeed", 0.3, writeDefault=False)
+    viser_hub_threshold = ntproperty("Properties/ViserHubThreshold", 0.05, writeDefault=False)
+    viser_hub_x_offset = ntproperty("Properties/ViserHubXOffset", 0.05, writeDefault=False)
+
+
+values = Properties()
