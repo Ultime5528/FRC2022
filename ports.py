f72bc0c81289b1c86bd1aa2c4e4a1c3b78390b01
Étienne Thibault <4001491@keranna.qc.ca>
2022-03-07 17:53:42 -0500

    Changement des ports

--- ports.py
+++ ports.py
@@ -4,25 +4,26 @@ basepilotable_left_motor_2 = 2
 basepilotable_right_motor_1 = 3
 basepilotable_right_motor_2 = 4
 
-shooter_motor_1 = 5
-shooter_motor_2 = 6
+shooter_motor_gauche = 5
+shooter_motor_droit = 6
 shooter_backspin_motor = 7
 
-grimpeur_follower_motor = 8
-grimpeur_leader_motor = 9
-grimpeur_secondaire_motor_ = 10
-camera_hub = 0
-camera_cargo = 1
-
+grimpeur_moteur_principal_gauche = 8
+grimpeur_moteur_principal_droit = 9
+grimpeur_moteur_secondaire = 10
 
 # PWM
 intake_moteur_intake = 0
 intake_moteur_transporter = 1
 
 # DIO
-intake_sensor_intake = 0
-intake_sensor_transporter = 1
-grimpeur_switch_haut = 2
-grimpeur_switch_bas = 3
-grimpeur_switch_haut_secondaire = 4
-grimpeur_switch_bas_secondaire = 5
+intake_sensor_bas = 0
+intake_sensor_haut = 1
+grimpeur_switch_haut = 9
+grimpeur_switch_principal_bas = 2
+grimpeur_switch_secondaire_bas = 3
+grimpeur_switch_secondaire_haut = 4
+
+# USB
+camera_hub = 0
+camera_cargo = 1
