ff9bdd32ccbe438a9bca623af10ca9cf605d9bd7
Étienne Beaulac <etienne.beaulac@outlook.com>
2022-04-02 10:56:00 -0400

    Removed empty line

--- commands/intake/descendreintake.py
+++ commands/intake/descendreintake.py
@@ -8,6 +8,5 @@ class DescendreIntake(commands2.SequentialCommandGroup):
     def __init__(self, grimpeur_secondaire: GrimpeurSecondaire):
         super().__init__(
             BougerSecondaire.to_max(grimpeur_secondaire)
-
         )
         self.setName(self.__class__.__name__)
