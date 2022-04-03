a12a767ec094dc2b97e153412e47d6be4b4ee46b
Thomas McDonald <4001137@keranna.qc.ca>
2022-04-03 15:11:19 -0400

    WIP autoStartup

---
+++ autostartup.cmd
@@ -0,0 +1,4 @@
+call C:\Users\Utilisateur\miniconda3\Scripts\activate.bat C:\Users\Utilisateur\miniconda3
+call conda activate frc2022
+python utils\autostartsetup.py
+
--- utils/autostartsetup.py
+++ utils/autostartsetup.py
@@ -3,22 +3,44 @@ import os
 import time
 import webbrowser
 from colorama import Fore
-chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+from pathlib import Path
+import subprocess
+import mouse
+
+chrome_path = "C:/Program Files/Google/Chrome/Application/chrome.exe %s"
 
 os.system("cls")
-print(Fore.GREEN)
 os.startfile(r"C:\Program Files (x86)\FRC Driver Station\DriverStation.exe")
-os.system(r"C:\Users\Admin\Desktop\FRC2022\.dashboard\pynetworktables2js.exe --team 5528")
-print(Fore.BLUE + "Waiting" + Fore.RESET)
-time.sleep(1)
 
-try:
-    DriverStation = pygetwindow.getWindowsWithTitle("FRC Driver Station")[0]
-except IndexError:
-    print(Fore.RED + 'FRC Driver Station not detected')
+time.sleep(1)
+DriverStation = None
+while not DriverStation:
+    try:
+        DriverStation = pygetwindow.getWindowsWithTitle("FRC Driver Station")[0]
+    except IndexError:
+        print('FRC Driver Station not detected')
+        time.sleep(0.1)
 
 DriverStation.moveTo(0, 0)
+time.sleep(0.75)
+DriverStation.maximize()
 
+subprocess.Popen(str(Path(r"dashboard\pynetworktables2js.exe").absolute()) + " --team 5528")
 time.sleep(1)
-webbrowser.get(chrome_path).open(r"youtube.com")
-print(Fore.RESET)
+webbrowser.get(chrome_path).open_new_tab(r"http://localhost:8888")
+time.sleep(1)
+print(pygetwindow.getAllTitles())
+
+Browser = None
+while Browser is None:
+    try:
+        Browser = pygetwindow.getWindowsWithTitle("My Robot Dashboard! - Google Chrome")[0]
+        print(Browser)
+    except IndexError:
+        print('Chrome not detected')
+        time.sleep(0.1)
+
+print("Resizing Chrome")
+time.sleep(1)
+Browser.minimize()
+Browser.resizeTo(1546, 603)
